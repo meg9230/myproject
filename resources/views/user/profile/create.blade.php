44af97a4f0425db0924357a510a35c0ad9548b74
Megumi Kutsuzawa <mgm.ktzw07@gmail.com>
2020-09-18 05:27:55 +0000

    login/new_registration

---
+++ resources/views/user/profile/create.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.user')
+
+@section('title', 'プロフィールの新規作成')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>新規プロフィールを作成</h2>
+            </div>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/user/profile/edit.blade.php
@@ -0,0 +1,42 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <title>Profile Page Edit</title>
+    </head>
+    <body>
+        <div id="app">
+            <h1>プロフィール更新ページ</h1>
+                <!--ナビゲーションバー-->
+            <nav class="navbar navbar-expand-md navbar-dark navbar-laravel">
+                <div class="container">
+                    <a class="navbar-brand" href="{{ url('/') }}">
+                        {{ config('app.name', 'Laravel') }}
+                    </a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <!-- Left Side Of Navbar -->
+                        <ul class="navbar-nav mr-auto">
+                            <li>Navi List1</li>
+                            <li>Navi List2</li>
+                            <li>Navi List3</li>
+                        </ul>
+
+                        <!-- Right Side Of Navbar -->
+                        <ul class="navbar-nav ml-auto">
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+
+            <main class="py-4">
+                @yield('content')
+            </main>
+        </div>
+    </body>
+</html>
